3be16a4858f7c2d5531c78ed8d279860386d3d98
Alexander F. Rødseth <rodseth@gmail.com>
2022-08-23 22:52:18 +0200

    Update help text for flags

--- src/bin.rs
+++ src/bin.rs
@@ -7,20 +7,23 @@ use docopt::Docopt;
 const VERSION_STRING: &'static str = "open 0.0.1";
 const USAGE: &'static str = "
   Usage:
-    open [-a application] [-b bundle_identifier] [-D] [-e] [-t] [-f] [-F] [-W] [-R] [-n] [-g] [--args] <file>...
+    open [-a application] [-b bundle_identifier] [-D] [-e] [-t] [-f] [-F] [-W] [-R] [-n] [-g] [-h] [--args] <file>...
 
   Options:
-    -D            Ost.
-    -e            Ost.
-    -t            Ost.
-    -f            Ost.
-    -F            Ost.
-    -W            Ost.
-    -R            Ost.
-    -n            Ost.
-    -g            Ost.
+    -a            Path to the application to use for opening the file.
+    -b            .desktop file in /usr/share/applications to use for opening the file.
+    -D            Reveal the enclosing folder in Thunar.
+    -e            Open the file in $EDITOR.
+    -t            Open the file as MIME type text/plain.
+    -f            Read input from stdin and open in $EDITOR.
+    -F            Open the application without restoring windows.
+    -W            Wait until the application exists, if already open.
+    -R            Reveal the files in Thunar instead of opening them.
+    -n            Open a new instance of the application(s), even if running.
+    -g            Do not bring the application to the foreground.
+    -h            Search the header locations for a given header filename.
     --args        Ost.
-    -h --help     Show this screen.
+    --help     Show this screen.
     --version     Show version.
 ";
 
