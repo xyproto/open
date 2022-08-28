161cb024bd3ae531cced14dc37bff93abaa2e29d
Alexander F. Rødseth <rodseth@gmail.com>
2022-08-28 13:50:39 +0200

    Various

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ extern crate docopt;
 
 use docopt::Docopt;
 
-use std::fs;
-use std::process::Command;
+//use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
+use std::process::Command;
 
 const VERSION_STRING: &'static str = "open 0.0.1";
 const USAGE: &'static str = "
@@ -37,49 +37,73 @@ struct Args {
     flag_version: bool,
 }
 
-pub fn get_desktop_file(mime_type: String) -> String {
-    let cmd = Command::new("xdg-mime")
+pub fn desktop_file_from_mime_type(mime_type: &str) -> Option<String> {
+    let cmd_output = Command::new("xdg-mime")
         .arg("query")
         .arg("default")
         .arg(mime_type)
-        .output()
-        .unwrap();
-    String::from_utf8(cmd.stdout).unwrap()
+        .output();
+    if let Some(cmd_output) = cmd_output.ok() {
+        // return the command output as an Option<String>
+        Some(
+            String::from_utf8(cmd_output.stdout.to_owned())
+                .unwrap()
+                .trim()
+                .to_string(),
+        )
+    } else {
+        None
+    }
 }
 
 // full_path_to_desktop_file tries to find the path to the given desktop file,
 // for instance "geany.desktop" on the current system.
-pub fn full_path_to_desktop_file(desktop_file: String) -> Option<PathBuf> {
-
-	let path_strings = [
-	  "~/.local/share/applications/",
-	  "/usr/local/share/applications/",
-	  "/usr/share/applications/"
-	];
-
-	for path_string in path_strings {
-	  let path = Path::new(path_string).join(desktop_file.to_owned()).to_path_buf();
-	  if path.exists() {
-		  return Some(path);
-	  }
-	}
-
-	None
+pub fn full_path_to_desktop_file(desktop_file: &str) -> Option<PathBuf> {
+    let path_strings = [
+        "~/.local/share/applications/",
+        "/usr/local/share/applications/",
+        "/usr/share/applications/",
+    ];
+    for path_string in path_strings {
+        let path = Path::new(&path_string).join(&desktop_file.trim());
+        if path.is_file() {
+            println!("{} exists", path.display());
+            return Some(path);
+        } else {
+            println!("{} does not exist", path.display());
+        }
+    }
+    None
 }
 
 fn main() {
-    let desktop_file_for_text_plain = get_desktop_file("text/plain".to_string());
-    println!("for text/plain: {}", desktop_file_for_text_plain);
-    let maybe_path = full_path_to_desktop_file(desktop_file_for_text_plain.to_owned());
+    let maybe_desktop_file = desktop_file_from_mime_type("text/plain");
 
-	let path_string = desktop_file_for_text_plain.to_owned();
-	let apath = maybe_path.unwrap_or(Path::new(&path_string).to_path_buf());
+    if let Some(desktop_file) = maybe_desktop_file {
+        println!("for text/plain: {}", desktop_file);
+        let maybe_path_buf = full_path_to_desktop_file(&desktop_file);
+        if let Some(path_buf) = maybe_path_buf {
+            println!("for text/plain: {}", path_buf.display());
+        } else {
+            println!("could not find the path for {}", desktop_file);
+            std::process::exit(1);
+        }
+    } else {
+        println!("could not find desktop file for text/plain");
+        std::process::exit(1);
+    }
 
-    println!("for text/plain: {:?}", apath);
-    println!("for text/plain: {:?}", apath.display());
-    println!("for text/plain: {:?}", &apath.display());
-    println!("for text/plain: {:?}", fs::canonicalize(&apath));
+    //     if let None = maybe_desktop_file_for_text_plain {
+    //     	println!("ost");
+    //     	std::process::exit(1);
+    //     }
+    //     let maybe_path = full_path_to_desktop_file(maybe_desktop_file_for_text_plain.unwrap().to_owned());
 
+    //     let apath = maybe_path.unwrap_or(Path::new(&maybe_path.unwrap().to_owned()).to_path_buf());
+    //     println!("for text/plain: {:?}", apath);
+    //     println!("for text/plain: {:?}", apath.display());
+    //     println!("for text/plain: {:?}", &apath.display());
+    //     println!("for text/plain: {:?}", fs::canonicalize(&apath));
 
     let args: Args = Docopt::new(VERSION_STRING.to_owned() + "\n" + USAGE)
         .and_then(|d| d.deserialize())
